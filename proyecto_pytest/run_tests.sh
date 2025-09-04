a851adbac69866206202771f2bfc480a6f7401d0
Sergio Alcón <104379849+SergioAlcon@users.noreply.github.com>
2025-09-04 20:52:47 +0200

    Entorno virutal correciones

--- proyecto_pytest/run_tests.sh
+++ proyecto_pytest/run_tests.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
+echo "Iniciando ejecución de pruebas en Jenkins..."
+
+# Verificar si el entorno virtual existe
 echo "activando el entorno virutal"
-if [! "-d venv"]; then
+if [! -d "venv"]; then
+    echo "Entorno virtual no encontrado. Creándolo..."
     python3 -m venv venv
 fi
 
-source venv/bin/activate
+# Activar el entorno virutal correctamente
+if [ -f "venv/bin/activate"]; then
+    source venv/bin/activate
+elif [ -f "venv/bin/activate"]; then #para windows
+    source venv/bin/activate
+else
+    echo "Error: No se pudo activar el entorno virutal."
+    exit 1
+fi
 
-echo "instalando dependencias"
-pip install --upgrade
-pip install -r requirements.txt
+# Verificar si `pip` está instalando correctamente
+echo "instalando dependencias..."
+pip install --upgrade pip --breack-system-packages
+pip install -r requirements.txt --breack-system-packages
 
-echo "ejecutando pruebas con pytest"
-pytest tests/ --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
+# Ejecutar las pruebas
+echo "ejecutando pruebas con pytest..."
+pytest tests/ --junitxml=reports/test-results.xml --html=reports/report.html --self-contained-html
 
 echo "pruebas finalizadas resultados en reports"
